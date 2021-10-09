e7786d68f5ca9e5fbad88e3dc68a11b963716fc1
sergio asbun fernandez <a2016114282@estudiantes.upsa.edu.bo>
2021-10-09 18:49:43 -0400

    ´Se agrego el contexto de datos

---
+++ MusicaParcial/Models/DätaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicaParcial.Models
+{
+    public class DätaContext:DbContext
+    {
+        public DätaContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;");
+        }
+    }
+}
