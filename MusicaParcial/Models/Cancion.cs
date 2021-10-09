20242866424c75ec4289f55d0f6de72688495a70
sergio asbun fernandez <a2016114282@estudiantes.upsa.edu.bo>
2021-10-09 18:12:57 -0400

    Agregando modelo de Cancion

---
+++ MusicaParcial/Models/Cancion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicaParcial.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public String Nombre { get; set; }
+        [Required]
+        public String Autor { get; set; }
+        [Required]
+        public String Letra { get; set; }
+        [Required]
+        public String EnlaceYT { get; set; }
+    }
+}
